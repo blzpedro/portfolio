90717fb3ac43ec98dcbb429d401a6cefedd6d030
Pedro Melo <phenriqmelo99@gmail.com>
2019-04-19 11:48:10 -0300

    responsivo lado direito

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-$("h3").click(function(){
-    alert("aaa")
+    $(".aba2").click(function () {
+      $(".aba1").removeClass("aba-ativa");
+      $(".aba2").addClass("aba-ativa");
+      $(".painel-portfolio").css("display", "block");
+      $(".painel-infos").css("display", "none");
+  });
+
+  $(".aba1").click(function () {
+      $(".aba2").removeClass("aba-ativa");
+      $(".aba1").addClass("aba-ativa");
+      $(".painel-infos").css("display", "block");
+      $(".painel-portfolio").css("display", "none");
   });
