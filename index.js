0982f12e3f46cd0c1611ad87b6359a7c751faf2e
Pedro Melo <phenriqmelo99@gmail.com>
2019-04-21 20:33:11 -0300

    muitas mudanças

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
     $(".aba2").click(function () {
       $(".aba1").removeClass("aba-ativa");
+      $(".aba2").removeClass("aba-hover");
       $(".aba2").addClass("aba-ativa");
+      $(".aba1").addClass("aba-hover");
       $(".painel-portfolio").css("display", "block");
       $(".painel-infos").css("display", "none");
   });
 
   $(".aba1").click(function () {
       $(".aba2").removeClass("aba-ativa");
+      $(".aba1").removeClass("aba-hover");
       $(".aba1").addClass("aba-ativa");
+      $(".aba2").addClass("aba-hover");
       $(".painel-infos").css("display", "block");
       $(".painel-portfolio").css("display", "none");
   });
